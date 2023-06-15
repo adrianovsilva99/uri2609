388dae97f9c4f8695306638d641a73e6d6b2184b
Adriano Vicente <adrianovic.silva@gmail.com>
2023-06-15 16:37:58 -0300

    first commit

---
+++ src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
@@ -0,0 +1,23 @@
+package com.devsuperior.uri2609.repositories;
+
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.uri2609.entities.Category;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query(nativeQuery = true, value = "SELECT categories.name, sum(products.amount) " +
+            "FROM categories " +
+            "INNER JOIN products ON categories.id = products.id_categories " +
+            "GROUP BY categories.name")
+    List<CategorySumProjection> search1();
+
+    @Query("SELECT new com.devsuperior.uri2609.dto.CategorySumDTO(obj.category.name, SUM(obj.amount)) " +
+            "FROM Product obj " +
+            "GROUP BY obj.category.name")
+    List<CategorySumDTO> search2();
+}
